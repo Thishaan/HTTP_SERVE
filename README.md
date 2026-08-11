dedd965e8083dd083ac3d448db9b5916a0bccbe6
Thishaan <udia@wisc.edu>
2026-08-11 16:47:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+General Workflow
+    1. socket()
+    2. bind()
+    3. listen()
+    4. accept()
+    5. recv() HTTP request
+    6. send() HTTP response
+    7. close()
+
+Later:
+
+    Test with 
---
+++ http_server.hpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+#include <string>
+//#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+
+
+
+/
+
+#ifndef INCLUDED_HTTP_SERVER
+#define INCLUDED_HTTP_SERVER
+
+
+
+class http_server{
+
+
+
+
+
+
+}
